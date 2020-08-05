35770ff5ea6d2881e4a674eb7e6d0a3ab7626a9a
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-06 00:20:49 +0800

    [ToDoList][James] feature:add  should_add_toDo_when_hit_post_toDo_endpoints_given_new_employee_info test

--- src/test/java/com/oocl/todolist/IntegrationTest/ToDoIntegrationTest.java
+++ src/test/java/com/oocl/todolist/IntegrationTest/ToDoIntegrationTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -15,6 +16,7 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -45,6 +47,24 @@ public class ToDoIntegrationTest {
                 .andExpect(jsonPath("$[0].id").value(1))
                 .andExpect(jsonPath("$[1].id").value(2))
                 .andExpect(jsonPath("$[2].id").value(3));
+        //then
+    }
+
+    @Test
+    void should_add_toDo_when_hit_post_toDo_endpoints_given_new_employee_info() throws Exception {
+        //given
+        String toDoInfo="{\n" +
+                "    \"content\":\"content33\",\n" +
+                "    \"status\":false,\n" +
+                "}";
+
+        //when
+        mockMvc.perform(post(("/todos")).contentType(MediaType.APPLICATION_JSON)
+                .content(toDoInfo))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNumber())
+                .andExpect(jsonPath("$.content").value("content1"))
+                .andExpect(jsonPath("$.status").value("false"));
         //then
     }
 }
