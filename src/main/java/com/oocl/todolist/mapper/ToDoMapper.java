cdfb2f65f24291d6d6283af9862087c888a1801b
CHENJA28 <CHENJA28@OOCL.COM>
2020-08-06 20:41:01 +0800

    [ToDoList][James] feature:implement should_attribute_equal_when_convert_to_do_to_to_do_response_given_to_do test

--- src/main/java/com/oocl/todolist/mapper/ToDoMapper.java
+++ src/main/java/com/oocl/todolist/mapper/ToDoMapper.java
@@ -6,7 +6,9 @@ public class ToDoMapper {
 
     public ToDoResponse covertToDoToToDoResponse(ToDo toDo) {
         ToDoResponse toDoResponse=new ToDoResponse();
-
+        toDoResponse.setContent(toDo.getContent());
+        toDoResponse.setId(toDo.getId());
+        toDoResponse.setStatus(toDoResponse.isStatus());
         return toDoResponse;
     }
 }
--- src/test/java/com/oocl/todolist/mapper/ToDoListMapperTest.java
+++ src/test/java/com/oocl/todolist/mapper/ToDoListMapperTest.java
@@ -21,4 +21,5 @@ public class ToDoListMapperTest {
         assertEquals(toDo.getContent(),toDoResponse.getContent());
         assertEquals(toDo.isStatus(),toDoResponse.isStatus());
     }
+
 }
